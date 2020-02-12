6d75aeb5ca6db86f1823076211ae507713cf4e41
Robert Parker <parker484@ufl.edu>
2020-02-12 02:20:19 -0500

    should be fixed

--- triangulate.c
+++ triangulate.c
@@ -2,17 +2,19 @@
 #include <math.h>
 
 //Globals
-float F[2];
-float Jac[2][2];
-float Inverse[2][2];
-float Result[2];
+double F[2];
+double Jac[2][2];
+double Inverse[2][2];
+double Result[2];
+int angle;
 
-void getFunction(float x, float y, float r1, float r2, float xa, float ya, float xb, float yb, float xc, float yc){
-	F[0] = sqrt(pow(x-xb, 2) + pow(x-yb,2)) - sqrt(pow(x-xa, 2) + pow(x-ya,2)) - r1;
-	F[1] = sqrt(pow(x-xc, 2) + pow(x-yc,2)) - sqrt(pow(x-xa, 2) + pow(x-ya,2)) - r2;
+void getFunction(double x, double y, double r1, double r2, double xa, double ya, double xb, double yb, double xc, double yc){
+	F[0] = sqrt(pow(x-xb, 2) + pow(y-yb,2)) - sqrt(pow(x-xa, 2) + pow(y-ya,2)) - r1;
+	F[1] = sqrt(pow(x-xc, 2) + pow(y-yc,2)) - sqrt(pow(x-xa, 2) + pow(y-ya,2)) - r2;
+	printf("%f", r1);
 }
 
-void getJacobian(float x, float y, float xa, float ya, float xb, float yb, float xc, float yc){
+void getJacobian(double x, double y, double xa, double ya, double xb, double yb, double xc, double yc){
 	//Calculate derivative 
 	Jac[0][0] = (x-xb)*(pow(pow(x-xb, 2) + pow(y-yb, 2), -0.5)) - (x-xa)*(pow(pow(x-xa,2) + pow(y-ya,2),-0.5));
 	Jac[0][1] = (y-yb)*(pow(pow(x-xb, 2) + pow(y-yb, 2), -0.5)) - (y-ya)*(pow(pow(x-xa,2) + pow(y-ya,2),-0.5));
@@ -22,13 +24,13 @@ void getJacobian(float x, float y, float xa, float ya, float xb, float yb, float
 
 void getInverse2x2(){
 	//Get a,b,c,d for ease of use
-	float a = Jac[0][0];
-	float b = Jac[0][1];
-	float c = Jac[1][0];
-	float d = Jac[1][1];
+	double a = Jac[0][0];
+	double b = Jac[0][1];
+	double c = Jac[1][0];
+	double d = Jac[1][1];
 
 	//Calculate determinant
-	float det = 1/(a*d - b*c);
+	double det = 1/(a*d - b*c);
 
 	//Invert it
 	Inverse[0][0] = d*det;
@@ -43,14 +45,14 @@ void mult2x2Matrix(){
 	Result[1] = Inverse[1][0]*F[0] + Inverse[1][1]*F[1];
 }
 
-float calculateError(float xOld, float yOld, float x, float y){
-	float temp = pow(x-xOld, 2);
+double calculateError(double xOld, double yOld, double x, double y){
+	double temp = pow(x-xOld, 2);
 	temp += pow(y-yOld, 2);
 
 	return temp;
 }
 
-int getAngle(float x, float y){
+int getAngle(double x, double y){
 	int quad;
 
 	//Get quadrant of coordinates
@@ -68,29 +70,33 @@ int getAngle(float x, float y){
 	}
 
 	//Compute angle in degrees
-	float rad2Deg = 180/3.14f;
-	return (int)(atan(fabs(x)/fabs(y))*rad2Deg + quad);
+	double rad2Deg = 180.0/3.14;
+	return (int)(angle + quad);
 }
 
 //maybe put a thermistor here
-float getTemperature(){
+double getTemperature(){
 	return 25;
 }
 
-float getSpeedOfSound(){
-	return (331 + 0.6*getTemperature());
+double getSpeedOfSound(){
+	return 343.0;
 }
 
-int triangulate(float xa, float ya, float xb, float yb, float xc, float yc, int ta, int tb, int tc){
+int triangulate(double xa, double ya, double xb, double yb, double xc, double yc, double ta, double tb, double tc){
 
 	//Variables
-	float x,y = 1.0f;
-	float err = 0;
-	float rAB = getSpeedOfSound()*(tb - ta);
-	float rAC = getSpeedOfSound()*(tc - ta);
+	double x = 1.0;
+	double y = 1.0;
+	double xOld = x;
+	double yOld = y;
+	double err = 1;
+	double rAB = getSpeedOfSound()*(tb - ta);
+	printf("%f", rAB);
+	double rAC = getSpeedOfSound()*(tc - ta);
 
 
-	while(err < 0.1){
+	while(err > 0.000001){
 		//Calculate function into F
 		//F =
 		getFunction(x, y, rAB, rAC, xa, ya, xb, yb, xc, yc);
@@ -98,6 +104,9 @@ int triangulate(float xa, float ya, float xb, float yb, float xc, float yc, int 
 		//Calculate derivatives into Jac
 		//Jac =
 		getJacobian(x, y, xa, ya, xb, yb, xc, yc);
+
+		printf("[%f, %f]\n", F[0], F[1]);
+		printf("[%f, %f, %f, %f]\n", Jac[0][0], Jac[0][1], Jac[1][0], Jac[1][1]);
 
 		//Place inverse of Jac into Inverse
 		//Inverse =
@@ -107,17 +116,26 @@ int triangulate(float xa, float ya, float xb, float yb, float xc, float yc, int 
 		//Result = 
 		mult2x2Matrix();
 
+		x = x - Result[0];
+		y = y - Result[1];
+
 		//Calculate error between new and old values
-		err = calculateError(x, y, Result[0], Result[1]);
+		err = calculateError(xOld, yOld, x, y);
+
+		printf("err: %f\n", err);
 
 		//Update values
-		x = Result[0];
-		y = Result[1];
+		xOld = x;
+		yOld = y;
 	}
+	printf("X: %f Y: %f\n", x, y);
 	//Note: this returns the angle from the perspective of the Mic which received the sound first
-	return getAngle(x,y);
+	angle = getAngle(x,y);
+	printf("angle: %d", angle);
 }
 
 int main(){
-
+	//triangulate(-0.10, -0.0866, 0.010, -0.0866, 0.0, 0.0866, 0.000023, 0.000039, 0.000045);
+	triangulate(0.0, -2.0, -3.0, 2.0, 3.0, 2.0, 0.00583, 0.01505, 0.01505);
+	printf("angle: %d", angle);
 }
--- triangulate.py
+++ triangulate.py
@@ -15,15 +15,14 @@ def lorenzSysNR(X, C, tauAB, tauAC, xa, ya, xb, yb, xc, yc):
 	y = X[1]
 
 	#equations for fval
-	eq1 = math.sqrt((x - xb)**2 + (x - yb)**2) - math.sqrt((x - xa)**2 + (x - ya)**2) - C*tauAB
-	eq2 = math.sqrt((x - xc)**2 + (x - yc)**2) - math.sqrt((x - xa)**2 + (x - ya)**2) - C*tauAC
-
+	eq1 = math.sqrt((x - xb)**2 + (y - yb)**2) - math.sqrt((x - xa)**2 + (y - ya)**2) - C*tauAB
+	eq2 = math.sqrt((x - xc)**2 + (y - yc)**2) - math.sqrt((x - xa)**2 + (y - ya)**2) - C*tauAC
+	print(C*tauAB)
 	#equations for jacobian
 	der1_1 = (x - xb)*(((x - xb)**2 + (y - yb)**2)**(-0.5)) - (x - xa)*(((x - xa)**2 + (y - ya)**2)**(-0.5)) 
 	der1_2 = (y - yb)*(((x - xb)**2 + (y - yb)**2)**(-0.5)) - (y - ya)*(((x - xa)**2 + (y - ya)**2)**(-0.5))
 	der2_1 = (x - xc)*(((x - xc)**2 + (y - yc)**2)**(-0.5)) - (x - xa)*(((x - xa)**2 + (y - ya)**2)**(-0.5))
 	der2_2 = (y - yc)*(((x - xc)**2 + (y - yc)**2)**(-0.5)) - (y - ya)*(((x - xa)**2 + (y - ya)**2)**(-0.5))
-
 
 	fval = np.array([eq1, eq2])
 	fval = fval.reshape((2,1))
@@ -32,6 +31,9 @@ def lorenzSysNR(X, C, tauAB, tauAC, xa, ya, xb, yb, xc, yc):
 
 	fval = fval.astype(float)
 	jac = jac.astype(float)
+
+	print(fval)
+	print(jac)
 
 	return fval, jac
 
@@ -58,6 +60,8 @@ def multiVarNR(xa, ya, xb, yb, xc, yc, ta, tb, tc):
 		X = X - np.matmul(np.linalg.inv(j), f)
 
 		err = abs(X - Xold)
+		print("err)")
+		print(err)
 
 		Xold = X
 
@@ -70,7 +74,8 @@ def multiVarNR(xa, ya, xb, yb, xc, yc, ta, tb, tc):
 
 
 def main():
-	X = multiVarNR(-0.10, -0.0866, 0.010, -0.0866, 0.0, 0.0866, 0.000023, 0.000039, 0.000045)
+	#X = multiVarNR(0.0, -0.0866, -0.10, 0.0866, 0.1, 0.0866, 0.000023, 0.000039, 0.000045)
+	X = multiVarNR(0.0, -2.0, -3.0, 2.0, 3.0, 2.0, 0.00583, 0.01505, 0.01505)
 	print(X)
 
 
